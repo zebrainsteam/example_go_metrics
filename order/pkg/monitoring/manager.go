42d665f4062463678b9519ff630ed5fb035aa0f4
Sergey <Sergey>
2023-04-23 18:51:59 +0400

    add comments

--- order/pkg/monitoring/manager.go
+++ order/pkg/monitoring/manager.go
@@ -28,6 +28,11 @@ func StartMetrics() (Metrics, error) {
 		Не самый полезный тип метрики, поскольку счетчик этот является неубывающим.
 		То есть, подходит он для отображения только чего-то вроде суммарного числа учетных записей в системе, да и то лишь при условии, что учетные записи являются неудаляемыми.
 	*/
+	/*
+		# HELP request_send Количество вызовов запросов
+		# TYPE request_send counter
+		request_send{type="какой то тип запроса"} 5
+	*/
 	createOrderSend := prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "example_go_metrics_orders",
 		Name:      "request_send",
@@ -41,6 +46,11 @@ func StartMetrics() (Metrics, error) {
 		Этот тип отлично подходит для отображения текущего значения чего-то
 		— температуры, давления, числа пользователей онлайн, и так далее.
 	*/
+	/*
+		# HELP work_order_create Количество активных вызовов создания заказа
+		# TYPE work_order_create gauge
+		work_order_create 3
+	*/
 	workOrderCreate := prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Namespace: "example_go_metrics_orders",
@@ -49,33 +59,64 @@ func StartMetrics() (Metrics, error) {
 		})
 	counters.Gauge["work_order_create"] = workOrderCreate
 	/*
-		Summary честно считает заданные процентили.
-		Идеально подходит для измерения времени ответа или чего-то такого.
-		Минус Summary заключается в том, что его дорого считать.
-		Поэтому часто обходятся гистограммами и примерными значениями процентилей.
-	*/
-	requestProcessingTimeSummaryMs := prometheus.NewSummary(
-		prometheus.SummaryOpts{
-			Namespace:  "example_go_metrics_orders",
-			Name:       "request_processing_time_summary_ms",
-			Help:       "Продолжительность исполнения запроса Summary",
-			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
-		})
-	counters.Summary["request_processing_time_summary_ms"] = requestProcessingTimeSummaryMs
-	/*
 		Histogram представляет собой гистограмму.
 		Этот тип метрики хранит число раз, которое измеряемая величина попала в заданный интервал значений (бакет).
 		Гистограммы может быть трудновато использовать, если интервал допустимых значений величины заранее неизвестен.
 		В Grafana по гистограмме можно примерно посчитать процентили, используя функцию histogram_quantile.
 	*/
+	/*
+		# HELP request_processing_time_histogram_ms Продолжительность выполнения запроса
+		# TYPE request_processing_time_histogram_ms histogram
+		request_processing_time_histogram_ms_bucket{status="какой то статус", method="какой то метод", le="0.1"} 0
+		request_processing_time_histogram_ms_bucket{status="какой то статус", method="какой то метод", le="0.15"} 0
+		request_processing_time_histogram_ms_bucket{status="какой то статус", method="какой то метод", le="0.2"} 0
+		request_processing_time_histogram_ms_bucket{status="какой то статус", method="какой то метод", le="0.25"} 0
+		request_processing_time_histogram_ms_bucket{status="какой то статус", method="какой то метод", le="0.3"} 0
+		request_processing_time_histogram_ms_bucket{status="какой то статус", method="какой то метод", le="+Inf"} 2
+		request_processing_time_histogram_ms_sum 3.5001457279999997
+		request_processing_time_histogram_ms_count 2
+	*/
+
 	requestProcessingTimeHistogramMs := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Namespace: "example_go_metrics_orders",
 			Name:      "request_processing_time_histogram_ms",
 			Help:      "Продолжительность исполнения запроса Histogram",
-			Buckets:   []float64{0.1, 0.15, 0.2, 0.25, 0.3},
+			// Можно указать свой тип бакета или оставить значение по умолчанию
+			// Buckets: []float64{0.5, 10, 20},
+			// Buckets: ExponentialBuckets(100, 1.2, 3),
+			// Buckets: LinearBuckets(-15, 5, 6),
+			Buckets: []float64{0.1, 0.15, 0.2, 0.25, 0.3},
 		}, []string{"status", "method"})
 	counters.Histogram["request_processing_time_histogram_ms"] = requestProcessingTimeHistogramMs
+	/*
+		Summary честно считает заданные процентили.
+		Идеально подходит для измерения времени ответа или чего-то такого.
+		Минус Summary заключается в том, что его дорого считать.
+		Поэтому часто обходятся гистограммами и примерными значениями процентилей.
+	*/
+	/*
+		# HELP request_processing_time_summary_ms Время задержки обработки запроса в секундах
+		# TYPE request_processing_time_summary_ms summary
+		request_processing_time_summary_ms{quantile="0.5"} 1.000199219
+		request_processing_time_summary_ms{quantile="0.9"} 2.5
+		request_processing_time_summary_ms{quantile="0.99"} 2.5
+		request_processing_time_summary_ms_sum 3.5001992189999998
+		request_processing_time_summary_ms_count 2
+	*/
+	requestProcessingTimeSummaryMs := prometheus.NewSummary(
+		prometheus.SummaryOpts{
+			Namespace: "example_go_metrics_orders",
+			Name:      "request_processing_time_summary_ms",
+			Help:      "Продолжительность исполнения запроса Summary",
+			/* В Objectives определяют оценки квантильного ранга с их соответствующими
+			абсолютными ошибками. Если Objectives[q] = e, то значение, сообщаемое для q
+			будет значением квантиля φ для некоторого φ между q-e и q+e.
+			Значение по умолчанию — пустая карта, что приводит к сводке без
+			квантили.*/
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		})
+	counters.Summary["request_processing_time_summary_ms"] = requestProcessingTimeSummaryMs
 	/*
 		Типы HistogramVec, SummaryVec и так далее.
 		Они представляют собой словарь (map) из описанных выше типов.
